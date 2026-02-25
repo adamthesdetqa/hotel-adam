f47d165dafd7c0531592f5658bd9358a5078140f
Adam Bethlehem <adam.bethlehem@safetyio.com>
2026-02-25 15:03:59 +0200

    Refactors reservation object creation
    Simplifies the creation of new reservation objects in the form submission handler.
    Utilizes the `ReservationInterface` for improved type safety and leverages the spread operator for cleaner assignment of form values.
    Adds default values to guest name and email fields for development convenience.

--- src/app/reservation-form/reservation-form.ts
+++ src/app/reservation-form/reservation-form.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule,FormGroup,FormBuilder, Validators } from '@angular/forms';
 import { checkOutAfterCheckIn } from '../validators/date.validators';
 import { ReservationsService } from '../services/reservations';
+import { ReservationInterface } from '../models/reservation';
 
 @Component({
   selector: 'app-reservation-form',
@@ -21,8 +22,8 @@ reservastionService = inject(ReservationsService);
   ngOnInit(): void {
       this.tomorrow.setDate(this.today.getDate() + 1);
       this.reservationForm = this.formBuilder.group({
-        guestName: ['', Validators.required],
-        guestEmail: ['', [Validators.required, Validators.email]],
+        guestName: ['Adam', Validators.required],
+        guestEmail: ['adam@adam.com', [Validators.required, Validators.email]],
         checkInDate: [this.today.toISOString().slice(0,10), Validators.required],
         checkOutDate: [this.tomorrow.toISOString().slice(0,10), Validators.required],
         numberOfGuests: ['1', [Validators.required, Validators.min(1)]]
@@ -30,15 +31,12 @@ reservastionService = inject(ReservationsService);
   }
   onSubmit(){
     if(this.reservationForm.valid){
-      const newReservation = {
-        id: crypto.randomUUID(),
-        guestName: this.reservationForm.value.guestName,
-        guestEmail: this.reservationForm.value.guestEmail,
-        checkInDate: new Date(this.reservationForm.value.checkInDate),
-        checkOutDate: new Date(this.reservationForm.value.checkOutDate),
-        numberOfGuests: this.reservationForm.value.numberOfGuests
+      const newReservation: ReservationInterface = {
+        ...this.reservationForm.value,
+        id: crypto.randomUUID()
       };
       console.log(this.reservationForm.value);
+      console.log(newReservation);
       this.reservastionService.addReservation(newReservation);
     } else{
       console.log('Form is invalid');
